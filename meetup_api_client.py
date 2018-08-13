86c8d56ca449422668408d35e6140cb7264c746d
3point14thon <chris.alan.bowers@gmail.com>
2018-08-13 14:33:40 -0700

    part way through reconfiguring api_client to get all requested data.

--- meetup_api_client.py
+++ meetup_api_client.py
@@ -1,5 +1,7 @@
 import requests
+from time import sleep
 from api_key import api_key
+import pymongo
 
 class MeetupApiClient:
     '''
@@ -15,6 +17,19 @@ class MeetupApiClient:
         input: api_method as a string,
                parameters as a dicitonary
         ouput: a url request object
+    get_groups,
+        input: parameters as a dicitonary
+        ouput: a url request object
+    get_events,
+        input: parameters as a dicitonary
+        ouput: a url request object
+    find_past_events,
+        input: location as a dictionary(to be used as parameters),
+               end_date as a string representing milliseconds from the epoch,
+               start_date as a string representing milliseconds from the epoch
+        output: all events in the specified location within the specified
+                location as a dictionary,
+                {group_name: [json dict of events, lat, lon]}
     '''
     def __init__(self):
         self.meetup_url = 'https://api.meetup.com/'
@@ -22,20 +37,29 @@ class MeetupApiClient:
 
     def get_item(self, api_method, parameters):
         parameters['key'] = api_key()
-        return requests.get(self.meetup_url + api_method, parameters)
+        res = requests.get(self.meetup_url + api_method, parameters)
+        if res.headers['X-RateLimit-Remaining'] == 0:
+            sleep(float(res.headers['X-RateLimit-Reset']))
+        return res
 
     def get_groups(self, params):
-        return self.get_item('2/groups', params)
+        #possible parameters are described here:
+        #https://www.meetup.com/meetup_api/docs/2/groups/
+        return self.get_item('find/groups', params)
 
     def get_events(self, params):
+        #possible parameters are described here:
+        #https://www.meetup.com/meetup_api/docs/2/events/
         return self.get_item('2/events', params)
 
-    def find_past_events(self, location, start_date, end_date):
+
+    def find_past_events(self, location, start_date, end_date, table):
         '''
         inconsistant on how many 400 errors are returned, might depend on
         internet connection
         '''
         group_events_dict = {}
+        location['only'] = 'lat,lon,urlname'
         content = self.get_groups(location)
         if content.status_code != requests.codes.ok:
             raise ValueError(str(response.status_code) +
@@ -46,8 +70,27 @@ class MeetupApiClient:
             group_name = group['urlname']
             params = {'time': '{},{}'.format(start_date, end_date),
                       'status': 'past',
-                      'group_urlname': group_name}
-            response = self.get_events(params)
-            self.url_code_log.append(response.status_code)
-            group_events_dict[group_name] = response.json()
+                      'group_urlname': group_name,
+                      'fields': '''event_hosts,
+                                   membership_d...,
+                                   series,
+                                   group_approved,
+                                   group_photo,
+                                   photo_album_id,
+                                   photo_count,
+                                   rsvp_rules,
+                                   survey_questions,
+                                   timezone,
+                                   comment_count'''}
+            if not table.find_one({'urlname': group_name}):
+                #consider putting a while not urlcode 200 here but be carful
+                response = self.get_events(params)
+                n = 0
+                while response.status_code == 400 and n != 100:
+                    response = self.get_events(params)
+                    n += 1
+                self.url_code_log.append(response.status_code)
+                table.insert_one({'urlname': group_name,
+                                  'data': response.json(),
+                                  'urlcode': response.status_code})
         return group_events_dict
