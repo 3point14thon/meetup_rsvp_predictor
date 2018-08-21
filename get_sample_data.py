3b9139f8407f4d306a91e095483cdcf0b7850a41
3point14thon <chris.alan.bowers@gmail.com>
2018-08-21 13:58:18 -0700

    file that gets random data from data base, file with 50% of the data

--- get_sample_data.py
+++ get_sample_data.py
@@ -1,11 +1,18 @@
 from pymongo import MongoClient
+import numpy as np
 
-def get_data_from_mongo():
+def get_data_from_mongo(percent_data):
     mc = MongoClient()
     db = mc['meetups']
     ed = db['events_data']
     num_events = ed.estimated_document_count()
     data = []
     count = 0
+    np.random.seed(1969)
+    nums = np.random.randint(0, num_events, int(num_events * percent_data))
+    #import pdb; pdb.set_trace()
     for event in ed.find():
-        if count in 
+        if count in nums:
+            data.append(event)
+        count += 1
+    return data
