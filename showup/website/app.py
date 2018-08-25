faab6f8fca42a5dca14285722affd9700e661b38
3point14thon <chris.alan.bowers@gmail.com>
2018-08-25 12:00:31 -0700

    website operates with no errors

--- showup/website/app.py
+++ showup/website/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, render_template, jsonify
-app = Flask(__name__)
 import pandas as pd
 import dill
-import meetup_pipeline
+from ..pipeline.meetup_pipes import (FeatureSelector, MapFeature,
+                                     CustomBinarizer, FillWith, RFRWrapper)
+from ..pipeline.meetup_pipeline import meetup_model, visibility
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -11,16 +14,15 @@ def index():
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    user_data = pd.DataFrame(request.json, index=[0])
-    user_data['waitlist_count'] = 0
-    prediction = predict(user_data)[0]
-    return jsonify({'prediction': prediction})
+     user_data = pd.DataFrame(request.json, index=[0])
+     prediction = predict(user_data)
+     return jsonify({'prediction': prediction})
 
 def predict(text_input):
     X = text_input
-    with open('static/model.pkl', 'rb') as f:
+    with open('showup/website/static/model.pkl', 'rb') as f:
         model = dill.load(f)
-    return model.predict(X)
+    return model.predict(X)[0]
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', threaded=True)
