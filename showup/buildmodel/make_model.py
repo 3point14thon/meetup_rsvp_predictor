a0ff55964587b2805ae1b91d7e5ff22e009b8dc7
3point14thon <chris.alan.bowers@gmail.com>
2018-08-25 10:26:11 -0700

    make_file works in current file schema

--- showup/buildmodel/make_model.py
+++ showup/buildmodel/make_model.py
@@ -1,11 +1,18 @@
-from ..pipeline.meetup_pipeline import meetup_model
+import sys
+sys.path.append("..")
+import pandas as pd
+from ..pipeline.meetup_pipeline import meetup_model, interest
+from .clean_data import drop_null_labels
 import dill
 
 def create_fit_pickle(X, y, filename):
     with open(filename, 'wb') as f:
-        model = meetup_model.fit(X, y)
+        model = meetup_model.fit_transform(X, y)
         dill.dump(model, f)
         return model
 
 if __name__ == '__main__':
-    create_fit_pickle(X, y, '../website/static/model.pkl')
+    meetup_df = pd.read_csv('data/data.gzip', compression='gzip')
+    X = drop_null_labels(meetup_df)
+    y = interest.fit_transform(X)
+    create_fit_pickle(X, y, 'showup/website/static/model.pkl')
