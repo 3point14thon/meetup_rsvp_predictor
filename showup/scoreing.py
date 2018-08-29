3794cb3aa253dbe897da8a9f558767536a8cf996
3point14thon <chris.alan.bowers@gmail.com>
2018-08-29 12:12:52 -0700

    changed x handling to use pandas, eleminated unneeded scorer

--- showup/scoreing.py
+++ showup/scoreing.py
@@ -1,17 +1,12 @@
 import numpy as np
 from sklearn.model_selection import KFold
 
-def neg_rmsle(y, y_pred):
-    log_pred = np.log(y_pred + 1)
-    log_y = np.log(y + 1)
-    return -(np.sum((log_pred-log_y)**2)/len(y))**0.5
-
 def cv_score(model, X, y, metric, splits=3, random_state=None, shuffle=False):
     kf = KFold(splits, shuffle, random_state)
     score = []
     for i_train, i_test in kf.split(X):
-        X_train = X[i_train]
-        X_test= X[i_test]
+        X_train = X.iloc[i_train]
+        X_test= X.iloc[i_test]
         y_train = y[i_train]
         y_test = y[i_test]
         model.fit(X_train, y_train)
