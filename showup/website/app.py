ffa2cdc8ceadb7227c282b21fcf8053ba859194b
3point14thon <chris.alan.bowers@gmail.com>
2018-08-29 16:42:37 -0700

    now converts log predictions to person count

--- showup/website/app.py
+++ showup/website/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import dill
 from ..pipeline.meetup_pipes import (FeatureSelector, MapFeature,
                                      CustomBinarizer, FillWith, RFRWrapper)
-from ..pipeline.meetup_pipeline import meetup_model, visibility
+from ..pipeline.meetup_pipeline import random_forest_model, visibility
 
 app = Flask(__name__)
 
@@ -15,6 +15,7 @@ def index():
 @app.route("/submit", methods=["POST"])
 def submit():
      user_data = pd.DataFrame(request.json, index=[0])
+     import pdb; pdb.set_trace()
      prediction = predict(user_data)
      return jsonify({'prediction': prediction})
 
@@ -22,7 +23,11 @@ def predict(text_input):
     X = text_input
     with open('showup/website/static/model.pkl', 'rb') as f:
         model = dill.load(f)
-    return model.predict(X)[0]
+    prediction = model.predict(X)[0]
+    return log_to_people(prediction)
+
+def log_to_people(x):
+    return np.e**x - 1
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
