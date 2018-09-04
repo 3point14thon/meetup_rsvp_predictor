6a220078042980b0948c0eb4bd97962c6cb262a3
3point14thon <chris.alan.bowers@gmail.com>
2018-09-04 09:27:02 -0700

    backing up untracked files before caching

--- showup/buildmodel/make_model.py
+++ showup/buildmodel/make_model.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("..")
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from ..pipeline.meetup_pipeline import random_forest_model, interest
 from .clean_data import drop_null_labels
 import dill
@@ -15,4 +16,5 @@ if __name__ == '__main__':
     meetup_df = pd.read_csv('data/data.gzip', compression='gzip')
     X = drop_null_labels(meetup_df)
     y = interest.fit_transform(X)
-    create_fit_pickle(X, y, 'showup/website/static/model.pkl')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1969)
+    create_fit_pickle(X_train, y_train, 'showup/website/static/model.pkl')
