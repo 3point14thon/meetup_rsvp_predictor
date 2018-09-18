a8b11a601719ba35f72695af45fa8c08c78fa310
3point14thon <chris.alan.bowers@gmail.com>
2018-09-18 15:28:26 -0700

    added venue and rsvp_rules tables

--- showup/data_base_setup/db_schema.sql
+++ showup/data_base_setup/db_schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE meetups;
 
 CREATE TABLE events (
-  id VARCHAR(20), figure out how long it should be set it to be index
+  id VARCHAR(20),
   created BIGINT,
   description TEXT,
   duration INT,
@@ -25,14 +25,46 @@ CREATE TABLE events (
   updated BIGINT,
   utc_offset INT,
   venue_id INT,
-  visibility VARCHAR(15)
+  visibility VARCHAR(14),
   waitlist_count INT,
   why TEXT,
   yes_rsvp_count INT
 );
 
-CREATE TABLE venue 
-CREATE TABLE rsvp_rules(link this with id)
+CREATE TABLE venue (
+  address_1 TEXT,
+  address_2 TEXT,
+  address_3 TEXT,
+  city VARCHAR(32),
+  country CHAR(2)
+  id INT,
+  Localized_country_name VARCHAR(32),
+  --lat and lon precision and scale based off this:
+  --https://en.wikipedia.org/wiki/Decimal_degrees#Precision
+  lat DOUBLE PRECISION,
+  lon DOUBLE PRECISION,
+  name TEXT,
+  phone VARCHAR(32),
+  repinned BOOLEAN,
+  state VARCHAR(2)
+  zip VARCHAR(16)
+);
+
+CREATE TABLE rsvp_rules (
+  event_id VARCHAR(20)
+  close_time BIGINT,
+  closed BOOLEAN,
+  guest_limit INT,
+  open_time BIGINT,
+  refund_days SMALLINT,
+  notes TEXT,
+  event_cancellation_refund BOOLEAN,
+  no_refund BOOLEAN,
+  member_cancellation_refund BOOLEAN,
+  event_rescheduled_refund BOOLEAN,
+  waitlisting VARCHAR(7)
+);
+
 CREATE TABLE event_hosts;
 CREATE TABLE featured_photo;
 CREATE TABLE fee(link this with id);
